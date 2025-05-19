4c31dc68c36c4ffee4e65d73ae555d81afc496da
Lisandro Rocha Tau <lumalisan@gmail.com>
2025-05-19 19:22:38 +0200

    feat: added login btn in desktop view

--- src/components/nav/LibraryLoginPopover.tsx
+++ src/components/nav/LibraryLoginPopover.tsx
@@ -5,11 +5,13 @@ import { faBook } from '@fortawesome/free-solid-svg-icons';
 interface LibraryLoginPopoverProps {
   triggerClassName?: string;
   side?: 'top' | 'right' | 'bottom' | 'left';
+  sideOffset?: number;
 }
 
 export const LibraryLoginPopover = ({
   triggerClassName,
   side,
+  sideOffset = 5,
 }: LibraryLoginPopoverProps) => {
   return (
     <Popover.Root>
@@ -26,7 +28,7 @@ export const LibraryLoginPopover = ({
       <Popover.Portal>
         <Popover.Content
           className='bg-secondary text-accent border-primary data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade z-50 w-64 rounded-md border p-5 shadow-lg'
-          sideOffset={5}
+          sideOffset={sideOffset}
           side={side}
           align='center'
         >
